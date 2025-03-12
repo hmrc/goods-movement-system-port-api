2a8f3a680721e1121a3c48c17cdc71f37b7502a3
Suresh Mandalapu <27204388+sureshhmrc@users.noreply.github.com>
2025-03-12 15:47:13 +0000

    [RORO-5737][SM]: Update bootstrap version (#15)

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,7 +2,7 @@ import sbt.*
 
 object AppDependencies {
 
-  private val bootstrapPlay30Version = "9.4.0"
+  private val bootstrapPlay30Version = "9.11.0"
   private val playVersion            = "play-30"
 
   val compile = Seq(
