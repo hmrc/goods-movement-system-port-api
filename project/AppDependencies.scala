aeb8423fcab633e9050fd452d13f14fc0fadfb5b
omirostrypatsas <115593155+omirostr1@users.noreply.github.com>
2026-03-24 14:03:43 +0000

    [RORO-6546][OT]upgrades bootstrap version

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,7 +2,7 @@ import sbt.*
 
 object AppDependencies {
 
-  private val bootstrapPlay30Version = "10.6.0"
+  private val bootstrapPlay30Version = "10.7.0"
   private val playVersion            = "play-30"
 
   val compile = Seq(
